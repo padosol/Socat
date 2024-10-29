8e15feffdd8e694656afba68c50a369362bdb5f3
한상훈 <hzyan2@gmail.com>
2024-10-29 12:48:28 +0900

    [room-service] Test: Room 생성 테스트 코드 작성

--- room-server/src/test/kotlin/com/room/roomservice/domain/room/domain/RoomTest.kt
+++ room-server/src/test/kotlin/com/room/roomservice/domain/room/domain/RoomTest.kt
@@ -1,23 +1,48 @@
 package com.room.roomservice.domain.room.domain
 
+import io.kotest.core.listeners.AfterTestListener
+import io.kotest.core.listeners.BeforeTestListener
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
+import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import org.junit.jupiter.api.Assertions.*
+import java.util.UUID
+
+
+class RoomIdGenerator : IdGenerator {
+    var id: String? = null
+    override fun createId(): String {
+        id = UUID.randomUUID().toString()
+        return id as String
+    }
+}
+
 
 class RoomTest : BehaviorSpec({
-    Given("방을 생성하려면") {
-        When("이러한 것이 있어야한다.") {
-            Then("그 결과 방이 생성된다."){
 
+    Given("방 인스턴스를 만든다.") {
+
+        val room = Room(
+            userId = "test",
+            roomName = "testRoom"
+        )
+
+        When("방을 생성한다.") {
+            val roomIdGenerator = RoomIdGenerator()
+            room.createRoom(roomIdGenerator)
+
+            Then("방 Id 가 생성된다."){
+                room.roomId shouldNotBe null
+                roomIdGenerator shouldNotBe null
+
+                room.roomId.shouldBe(roomIdGenerator.id)
             }
         }
     }
 
-    Given("방 삭제") {
-        When("삭제로직") {
-            Then("결과") {
-
-            }
-        }
-    }
 
 })
+
