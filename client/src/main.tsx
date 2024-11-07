0658d63750b8550d223d90be12d53629bea17f56
한상훈 <hzyan2@gmail.com>
2024-11-07 22:54:42 +0900

    Test: router test

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,20 +8,47 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import Root from "./routes/root";
+import Root, { 
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
 
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, {
+  loader as contactLoader
+} from "./routes/contact";
+
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oop! There was an error</div>,
       },
     ]
   },
