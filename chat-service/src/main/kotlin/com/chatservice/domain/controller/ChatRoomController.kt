a02d1cf23a3c0b5e6c1ebad66cd5a63ff66ac81f
한상훈 <hzyan2@gmail.com>
2024-11-12 11:02:34 +0900

    [chat-service] Fix: ChatController 수정
    controller -> rest-controller 로 변경

--- chat-service/src/main/kotlin/com/chatservice/domain/controller/ChatRoomController.kt
+++ chat-service/src/main/kotlin/com/chatservice/domain/controller/ChatRoomController.kt
@@ -2,62 +2,37 @@ package com.chatservice.domain.controller
 
 import com.chatservice.domain.dto.ChatRoomDTO
 import com.chatservice.domain.repository.ChatRoomRepository
-import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 @RequestMapping("/chat")
 class ChatRoomController(
     private val chatRoomRepository: ChatRoomRepository
 ) {
 
-    // 채팅 리스트 화면
-    @GetMapping("/room")
-    fun rooms(model: Model): String {
-        return "/chat/room"
-    }
-
     // 모든 채팅방 목록
     @GetMapping("/rooms")
-    @ResponseBody
     fun room(): MutableList<ChatRoomDTO> {
         return chatRoomRepository.findAllRoom()
     }
 
     // 채팅방 생성
     @PostMapping("/rooms")
-    @ResponseBody
     fun createRoom(@RequestParam("name") name: String): ChatRoomDTO {
         return chatRoomRepository.createChatRoom(name)
     }
 
-    //채팅방 입장 화면
-    @GetMapping("/room/enter/{roomId}")
-    fun roomDetail(
-        model: Model,
-        @PathVariable("roomId") roomId: String
-    ): String {
-        model.addAttribute("roomId", roomId)
-        return "/chat/roomDetail"
-    }
-
     // 특정 채팅방 조회
     @GetMapping("/rooms/{roomId}")
-    @ResponseBody
     fun roomInfo(
         @PathVariable("roomId") roomId: String
     ): ChatRoomDTO? {
         val findRoom = chatRoomRepository.findRoomById(roomId)
         return findRoom
     }
-
-
-
-
 }
