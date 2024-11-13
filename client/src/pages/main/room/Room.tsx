f5a5182408f62c34cd5c074c4aad1adedf5961a3
한상훈 <hzyan2@gmail.com>
2024-11-13 10:59:23 +0900

    [client] Refactor: Room 페이지 타입스크립트 추가

--- client/src/pages/main/room/Room.tsx
+++ client/src/pages/main/room/Room.tsx
@@ -1,26 +1,47 @@
-import { useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import {
   Link,
-  useLoaderData
+  useLoaderData,
+  LoaderFunctionArgs
 } from "react-router-dom"
 import * as StompJs from '@stomp/stompjs';
 
 import { getRoom } from "../../../api/room";
 
-export async function loader({params}) {
-  const response = await getRoom(params.roomId);
+interface RoomData {
+  name: string,
+  roomId: string
+}
 
+interface LoaderData {
+  room: RoomData
+}
+
+interface MyMessage {
+  type: string,
+  roomId: string,
+  sender: string,
+  message: string,
+  userCount: number
+}
+
+export async function loader({params}: LoaderFunctionArgs ) {
+  const roomId= params.roomId
+  if (!roomId) {
+    return {};
+  }
+
+  const response = await getRoom(roomId);
   const room = response.data;
-
   return {room};
 }
 
 const Room = () => {
-  const {room} = useLoaderData();
+  const {room} = useLoaderData() as LoaderData;
 
-  const wsClient = useRef<StompJs.Stomp>(null);
+  const wsClient = useRef<StompJs.CompatClient | null>(null);
   const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState<StompJs.IMessage[]>([]);
+  const [messages, setMessages] = useState<MyMessage[]>([]);
 
   const handleMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value)
@@ -31,27 +52,35 @@ const Room = () => {
     wsClient.current = StompJs.Stomp.over(client);
 
     wsClient.current.connect({}, () => {
-      wsClient.current.subscribe(`/sub/chat/room/${room.roomId}`, (message: StompJs.IMessage) => {
-        const newMessage = JSON.parse(message.body);
 
-        setMessages((messages) => [...messages, newMessage])
+      if(wsClient.current) {
+        wsClient.current.subscribe(`/sub/chat/room/${room.roomId}`, (message: StompJs.IMessage) => {
+          const newMessage = JSON.parse(message.body);
+  
+          setMessages((messages) => [...messages, newMessage])
+  
+        })
+  
+        wsClient.current.send("/pub/chat/message", {}, JSON.stringify({type: "JOIN", roomId: room.roomId, sender: "tester"}))
+      }
 
-      })
-
-      wsClient.current.send("/pub/chat/message", {}, JSON.stringify({type: "JOIN", roomId: room.roomId, sender: "tester"}))
     })
   }
 
   const sendMessage = () => {
     if(message) {
-      wsClient.current.send("/pub/chat/message", {}, JSON.stringify({type: "CHAT", roomId: room.roomId, sender: "tester", message: message}))
-      setMessage("")
+      if(wsClient.current) {
+        wsClient.current.send("/pub/chat/message", {}, JSON.stringify({type: "CHAT", roomId: room.roomId, sender: "tester", message: message}))
+        setMessage("")
+      }
     }
   }
 
   const disconnect = () => {
-    wsClient.current.send("/pub/chat/message", {}, JSON.stringify({type: "LEAVE", roomId: room.roomId, sender: "tester"}))
-    wsClient.current.disconnect();
+    if(wsClient.current) {
+      wsClient.current.send("/pub/chat/message", {}, JSON.stringify({type: "LEAVE", roomId: room.roomId, sender: "tester"}))
+      wsClient.current.disconnect();
+    }
   }
 
   useEffect(() => {
@@ -89,7 +118,12 @@ const Room = () => {
         }
       </div>
       <div className="mt-60">
-        <input className="border border-black p-1" onChange={handleMessageChange} value={message}></input>
+        <input 
+          className="border border-black p-1" 
+          onChange={handleMessageChange} 
+          value={message}
+          placeholder="채팅에 참여하려면 로그인을 해주세요."
+        ></input>
         <button className="ml-2 border border-black rounded-md hover:bg-slate-300 p-1" onClick={sendMessage}>전송</button>
       </div>
     </div>
