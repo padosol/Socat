636ee05e116d13ed70139ea7beecf219f02fa823
한상훈 <hzyan2@gmail.com>
2024-12-11 23:44:01 +0900

    user-service docker-compose 추가

--- user-service/src/main/java/com/userservice/global/config/redis/RedisConfig.java
+++ user-service/src/main/java/com/userservice/global/config/redis/RedisConfig.java
@@ -1,18 +1,24 @@
 package com.userservice.global.config.redis;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.port}")
+    public int port;
+
+    @Value("${spring.data.redis.host}")
+    public String host;
 
     @Bean
     public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
     }
 
 
