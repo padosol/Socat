238efb64aba87978a766b45a52e70c550142c776
한상훈 <hzyan2@gmail.com>
2025-01-03 23:20:52 +0900

    [user-service] Test: user controller 테스트 코드 작성

--- user-service/src/main/java/com/userservice/domain/user/controller/dto/request/CreateUserDTO.java
+++ user-service/src/main/java/com/userservice/domain/user/controller/dto/request/CreateUserDTO.java
@@ -22,4 +22,12 @@ public class CreateUserDTO {
     @Size(min = 8, max = 16, message = "비밀번호는 8자 이상 16자 이하 입니다.")
     @Schema(description = "유저 패스워드", example = "test1234")
     private String password;
+
+    private CreateUserDTO(){}
+
+    public CreateUserDTO(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 }
