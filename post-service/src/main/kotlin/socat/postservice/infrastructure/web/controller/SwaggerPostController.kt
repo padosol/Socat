df3ff1ea12dd5e36b31dc027fcd7c09839d3cf3a
한상훈 <hzyan2@gmail.com>
2025-02-07 23:55:52 +0900

    Feat: uplaod api 추가

--- post-service/src/main/kotlin/socat/postservice/infrastructure/web/controller/SwaggerPostController.kt
+++ post-service/src/main/kotlin/socat/postservice/infrastructure/web/controller/SwaggerPostController.kt
@@ -3,12 +3,11 @@ package socat.postservice.infrastructure.web.controller
 import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.multipart.MultipartFile
 import socat.postservice.global.dto.APIResponse
 import socat.postservice.infrastructure.web.dto.request.CreatePostDTO
 import socat.postservice.infrastructure.web.dto.request.ModifyPostDTO
 import socat.postservice.infrastructure.web.dto.request.RemovePostDTO
-import socat.postservice.infrastructure.web.dto.request.SearchPostDTO
 import socat.postservice.infrastructure.web.dto.response.PostResponse
 
 interface SwaggerPostController {
@@ -36,5 +35,9 @@ interface SwaggerPostController {
         page: Int,
         query: String,
     ): ResponseEntity<APIResponse<List<PostResponse>>>
+
+    fun imageUpload(
+        file: MultipartFile
+    ): ResponseEntity<APIResponse<String>>
 
 }
